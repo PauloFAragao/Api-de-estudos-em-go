ef22586faf00fe7c08feb0b37b4be76568559aeb
PauloFAragao <debek_metal@hotmail.com>
2025-01-10 16:03:45 -0300

    Aula: Página de Cadastro de Usuários

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -10,3 +10,9 @@ func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
 	// renderizando a pagina de login
 	utils.ExecutarTemplate(w, "login.html", nil)
 }
+
+// CarregarPaginaDeCadastroDeUsuario vai carregar a pagina de cadastro do usuário
+func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
+	// renderizando a pagina de login
+	utils.ExecutarTemplate(w, "cadastro.html", nil)
+}
