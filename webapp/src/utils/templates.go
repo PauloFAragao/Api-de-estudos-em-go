c5298fd7e21e06b76a5f7d4f91f4e5f53cc31835
PauloFAragao <debek_metal@hotmail.com>
2025-01-10 08:50:51 -0300

    Aula: Templates HTML

---
+++ webapp/src/utils/templates.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var templates *template.Template
+
+// CarregarTemplates insere os templates html na variável templates
+func CarregarTemplates() {
+	// carregando os arquivos html na variável
+	templates = template.Must(templates.ParseGlob("views/*.html"))
+}
+
+// ExecutarTemplate renderiza uma página html na tela
+func ExecutarTemplate(w http.ResponseWriter, template string, dados interface{}) {
+	templates.ExecuteTemplate(w, template, dados)
+}
