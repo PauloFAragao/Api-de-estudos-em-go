4583a10192a3cdb0891d4bdbf2b76dba8434974e
PauloFAragao <debek_metal@hotmail.com>
2025-01-13 06:33:04 -0300

    Aula: Renderizando Publicações na Página Principal

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"webapp/src/config"
+	"webapp/src/modelos"
 	"webapp/src/requisicoes"
+	"webapp/src/respostas"
 	"webapp/src/utils"
 )
 
@@ -26,8 +29,26 @@ func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
 
 	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodGet, url, nil)
-	fmt.Println(response.StatusCode, erro)
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+	defer response.Body.Close()
+
+	// verificando o status code do html
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeDeErro(w, response)
+		return
+	}
+
+	var publicacoes []modelos.Publicacao
+
+	// pegando as informações da publicação
+	if erro = json.NewDecoder(response.Body).Decode(&publicacoes); erro != nil {
+		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
 
 	// renderizando a pagina de login
-	utils.ExecutarTemplate(w, "home.html", nil)
+	utils.ExecutarTemplate(w, "home.html", publicacoes)
 }
